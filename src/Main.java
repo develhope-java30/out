161555dfd3bc36136cfbb5ac8fc65d496dc21fdb
merry <rozzimario18@gmail.com>
2024-12-10 10:14:20 +0100

    changed println

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        System.out.println("elena reas");
     }
-    }
+}
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
